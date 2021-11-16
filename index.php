f3c8abf4ddad064fc2d1e9aa3412cb3bd9f1a4b6
Max <kamingliow@gmail.com>
2021-11-17 07:15:30 +0800

    first commit

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
+
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
 <link rel="stylesheet" type="text/css" href="css/jquery.countdown.css">
 <script type="text/javascript" src="js/jquery.plugin.js"></script>
